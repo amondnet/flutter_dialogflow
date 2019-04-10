48008575b84658ed564a588b3855e139fce1d635
Minsu Lee <amond@amond.net>
2019-04-10 12:57:54 +0900

    support EventInput

--- lib/dialogflow_v2.dart
+++ lib/dialogflow_v2.dart
@@ -4,3 +4,6 @@ export 'v2/auth_google.dart';
 export 'v2/dialogflow_v2.dart';
 export 'utils/language.dart';
 export 'v2/message.dart';
+export 'v2/event_input.dart';
+export 'v2/query_input.dart';
+export 'v2/text_input.dart';
