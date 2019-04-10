366cb4fa50a1c385330cedd1c93099c64672095e
Minsu Lee <amond@amond.net>
2019-04-10 14:19:29 +0900

    support EventInput

---
+++ lib/v2/detect_intent.g.dart
@@ -0,0 +1,20 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'detect_intent.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+DetectIntentRequest _$DetectIntentRequestFromJson(Map<String, dynamic> json) {
+  return DetectIntentRequest(json['queryInput'] == null
+      ? null
+      : _dataFromJson(json['queryInput'] as Map<String, dynamic>));
+}
+
+Map<String, dynamic> _$DetectIntentRequestToJson(
+        DetectIntentRequest instance) =>
+    <String, dynamic>{
+      'queryInput':
+          instance.queryInput == null ? null : _dataToJson(instance.queryInput)
+    };
